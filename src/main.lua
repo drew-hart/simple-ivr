35129c658d90c2b53ea8dedbd2f04f31422d883f
Drew Hart <andrewmhart@gmail.com>
2015-02-25 15:02:05 -0600

    updated sound file

--- src/main.lua
+++ src/main.lua
@@ -1,12 +1,49 @@
 local menu = require "summit.menu"
 
 channel.answer()
-channel.play({'/sounds/a-team_intro.wav'})
 
+---------------------------
+--
+-- Logic for invalid option being pressed
+--
+---------------------------
+function invalid_input()
+	channel.say("The number you pressed as not valid. Please try again.")	
+end
+
+---------------------------
+--
+-- Logic for Sales group
+--
+---------------------------
+function sales()
+	channel.say("Dialing sales now.")
+	channel.dial('8172964129')
+end
+
+---------------------------
+--
+-- Logic for Support group
+--
+---------------------------
+function support()
+	channel.say("Dialing support now.")
+	channel.dial('8172964129')
+end
+
+---------------------------
+--
+-- Main
+--
+---------------------------
 local my_menu = menu.Menu()
-my_menu.add("1", "Press 1 to contact sales", play_messages)
-my_menu.add("2", "Press 2 to contact support", delete_all_messages)
-my_menu.default(invalid_input)
+my_menu.attempts(5)
+my_menu.timeout(60)
+my_menu.intro({'/sounds/intro.wav'})
+my_menu.add("1", "Press 1 to contact sales", sales)
+my_menu.add("2", "Press 2 to contact support", support)
+my_menu.invalid(SILENCE)
+my_menu.default()
 my_menu.run()
 
 channel.hangup()
