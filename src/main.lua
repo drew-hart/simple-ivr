15f96b394bd00c8327b3530d429814834b902fb2
Drew Hart <andrewmhart@gmail.com>
2015-02-25 14:14:08 -0600

    using menu class now

--- src/main.lua
+++ src/main.lua
@@ -1,17 +1,12 @@
 local menu = require "summit.menu"
 
-
 channel.answer()
 channel.play({'/sounds/a-team_intro.wav'})
-local digit = channel.gather({play="Press 1 for sales.  Press 2 to delete all messages", maxDigits=1})
-if digit == '1' then
-    return play_messages
-elseif digit == '2' then
-    return delete_all_messages
-else
-    return invalid_input
-end
 
+local my_menu = menu.Menu()
+my_menu.add("1", "Press 1 to contact sales", play_messages)
+my_menu.add("2", "Press 2 to contact support", delete_all_messages)
+my_menu.default(invalid_input)
+my_menu.run()
 
 channel.hangup()
-
