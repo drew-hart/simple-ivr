4aaa90d910734cf70d8818bf20b20a11a27a24d3
Drew Hart <andrewmhart@gmail.com>
2015-02-25 13:08:47 -0600

    added intro sound file

--- src/main.lua
+++ src/main.lua
@@ -1,9 +1,17 @@
--- This comment enforces unit-test coverage for this file:
--- coverage: 0
+local menu = require "summit.menu"
+
 
 channel.answer()
-channel.say("This is an example application. Drew. Enter a number followed by the pound sign. Enter it now.")
-local digits = channel.gather()
-channel.say(digits)
+channel.play({'/sounds/greeting.wav'})
+local digit = channel.gather({play="Press 1 for sales.  Press 2 to delete all messages", maxDigits=1})
+if digit == '1' then
+    return play_messages
+elseif digit == '2' then
+    return delete_all_messages
+else
+    return invalid_input
+end
+
+
 channel.hangup()
 
