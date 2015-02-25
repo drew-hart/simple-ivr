8c52e5ad0935ac562cb0381fdee3c3f83db47b90
Drew Hart <andrewmhart@gmail.com>
2015-02-25 16:13:21 -0600

    removed function for invalid input

--- src/main.lua
+++ src/main.lua
@@ -7,16 +7,6 @@ channel.answer()
 
 ---------------------------
 --
--- Logic for invalid option being pressed
---
----------------------------
-function invalid_input()
-	channel.say("The number you pressed is not a valid option")	
-	my_menu.run()
-end
-
----------------------------
---
 -- Logic for Sales group
 --
 ---------------------------
@@ -44,7 +34,6 @@ local my_menu = menu.Menu()
 my_menu.intro("Thanks for calling.")
 my_menu.add("1", "Press 1 to contact sales", sales)
 my_menu.add("2", "Press 2 to contact support", support)
-my_menu.invalid(invalid_input)
 my_menu.run()
 
 channel.hangup()
