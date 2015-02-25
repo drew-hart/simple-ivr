f0957da010a7a538e3c16d13d5debaa291737761
Drew Hart <andrewmhart@gmail.com>
2015-02-25 15:35:15 -0600

    added wav intro back

--- src/main.lua
+++ src/main.lua
@@ -35,7 +35,7 @@ end
 --
 ---------------------------
 local my_menu = menu.Menu()
-my_menu.intro("Thanks for calling.")
+my_menu.intro({'/sounds/intro.wav'})
 my_menu.add("1", "Press 1 to contact sales", sales)
 my_menu.add("2", "Press 2 to contact support", support)
 my_menu.default()
