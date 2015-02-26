e7ad4babb0316e9fe98a93ff1b1358fe2611036b
Drew Hart <andrewmhart@gmail.com>
2015-02-26 08:49:34 -0600

    still debugging

--- src/main.lua
+++ src/main.lua
@@ -5,6 +5,25 @@ local support_phone = '2622271450'
 
 channel.answer()
 
+---------------------------
+--
+-- Logic for Sales group
+--
+---------------------------
+function sales()
+	channel.say("Dialing sales now.")
+	channel.dial(sales_phone)
+end
+
+---------------------------
+--
+-- Logic for Support group
+--
+---------------------------
+function support()
+	channel.say("Dialing support now.")
+	channel.dial(support_phone)
+end
 
 ---------------------------
 --
@@ -12,10 +31,8 @@ channel.answer()
 --
 ---------------------------
 local my_menu = menu.Menu()
-my_menu.intro("Thanks for calling.")
 my_menu.add("1", "Press 1 to contact sales", sales)
 my_menu.add("2", "Press 2 to contact support", support)
-my_menu.invalid("The number you pressed is not a valid option")
 my_menu.run()
 
 channel.hangup()
