46b3920a2a2d3c531e0a712bd71e961b8586ec19
Drew Hart <andrewmhart@gmail.com>
2015-02-26 08:55:40 -0600

    added 1 second delay so prompts are heard

--- src/main.lua
+++ src/main.lua
@@ -1,4 +1,5 @@
 local menu = require "summit.menu"
+local time = require('summit.time')
 
 local sales_phone   = '8172964129'
 local support_phone = '2622271450'
@@ -11,6 +12,7 @@ channel.answer()
 --
 ---------------------------
 function sales()
+	time.sleep(1)
 	channel.say("Dialing sales now.")
 	channel.dial(sales_phone)
 end
@@ -21,6 +23,7 @@ end
 --
 ---------------------------
 function support()
+	time.sleep(1)
 	channel.say("Dialing support now.")
 	channel.dial(support_phone)
 end
@@ -34,7 +37,6 @@ local my_menu = menu.Menu()
 my_menu.intro("Thanks for calling.")
 my_menu.add("1", "Press 1 to contact sales", sales)
 my_menu.add("2", "Press 2 to contact support", support)
---my_menu.invalid("The number you pressed is not a valid option")
 my_menu.run()
 
 channel.hangup()
