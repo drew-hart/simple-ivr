ce92788ae41a9b27d4162f26241a25ec6627131a
Drew Hart <andrewmhart@gmail.com>
2015-02-27 15:00:03 -0600

    moved menu logic into a functino

--- src/main.lua
+++ src/main.lua
@@ -69,15 +69,25 @@ end
 
 ---------------------------
 --
--- Main
+-- Menu logic
 --
 ---------------------------
-local my_menu = menu.Menu()
-my_menu.intro("Thanks for calling.")
-my_menu.add("1", "Press 1 to contact sales", sales)
-my_menu.add("2", "Press 2 to contact support", support)
--- TODO: Add an invalid option using my_menu.default function
-my_menu.run()
+function my_menu()
+	local my_menu = menu.Menu()
+	my_menu.intro("Thanks for calling.")
+	my_menu.add("1", "Press 1 to contact sales", sales)
+	my_menu.add("2", "Press 2 to contact support", support)
+	-- TODO: Add an invalid option using my_menu.default function
+	my_menu.timeout(30)
+	my_menu.default(invalid_option)
+	my_menu.run()
+end
 
+---------------------------
+--
+-- Main 
+--
+---------------------------
+channel.answer()
 get_datastore_data()
 channel.hangup()
