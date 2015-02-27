13e772d3af20e05b51ca3a4f4d347d0ea774d019
Drew Hart <andrewmhart@gmail.com>
2015-02-27 17:13:20 -0600

    added documentation for setting up datastore

--- src/main.lua
+++ src/main.lua
@@ -6,8 +6,50 @@ local datastore = require "summit.datastore"
 local recording = require("summit.recording")
 local sound = require("summit.sound")
 
--- Static variables
--- TODO: Move to datastore
+-- ===================================================================
+--
+-- SETUP - Summit Datastore
+--
+-- Summit provides you with powerful database tool, where you can
+-- store data. You can access datastore through the Summit Dashboard. 
+-- To create a Summit account visit http://developers.corvisa.com
+-- 
+-- Several tables are required for this application to function. You
+-- create tables by logging into the dashboard and clicking on 
+-- Application > Datastore. From there select your application on the 
+-- drop down and create tables by clicking on the plus sign. Below are
+-- a list of tables that are used in this application to get you
+-- started, but you are welcome to change them to meet your needs.
+-- 
+-----------------------------------------
+--                TABLE 			   --
+-----------------------------------------
+-- 		Name: Group Numbers
+-- 		Type: String
+-- 		Key: sales      Value: [your phone number]
+-- 		Key: support    Value: [a phone number]
+--
+-----------------------------------------
+--                TABLE 			   --
+-----------------------------------------
+-- 		Name: Office Hours
+-- 		Type: Map
+-- 		Key Name: sunday     Key Values: name = start, value = [HHMM]
+--							 Key Values: name = end, value = [HHMM]
+-- 		Key Name: monday     Key Values: name = start, value = [HHMM]
+--							 Key Values: name = end, value = [HHMM]
+-- 		Key Name: tuesday    Key Values: name = start, value = [HHMM]
+--							 Key Values: name = end, value = [HHMM]
+--		... repeat this for each day of the week 
+-- 
+-----------------------------------------
+--                TABLE 			   --
+-----------------------------------------
+-- 		Name: Office is Closed Message
+-- 		Type: String
+-- 		Key: message     Value: [the message you want played]
+--
+-- ===================================================================
 
 ---------------------------
 --
